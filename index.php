e39f2dc5f80db872c97d39ab34e4f5a2a6ff0273
cmrwebdev <33638879+cmrweb@users.noreply.github.com>
2020-02-21 19:54:38 +0100

    fix

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>cmrTemplate engine</title>
+</head>
+<body>
+<?php
+
+require "Router.php";
+
+?>
+<script src="lib/cmrfor.js"></script>
+</body>
+</html>
