34c12f250cc1d6dc4f872e007bd24eeb04a7b259
cmrwebdev <33638879+cmrweb@users.noreply.github.com>
2020-02-21 21:48:56 +0100

    update

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>cmrTemplate engine</title>
+    <link rel="stylesheet" href="asset/css/cmrstyle.min.css">
 </head>
 
 <body>
